de1baae6efdad59a4661f739f8bcc666ec5be01a
Paulo Tada <paulohtfs@gmail.com>
2015-05-05 05:46:24 -0300

    2 math problems and 3 html problems

---
+++ ppc_problems/math_ad_hoc/faculdade_fisica.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int v, t;
+
+	while(cin >> v >> t){
+		long long res = (v*2)*t;
+		cout << res << endl;
+	}
+	return 0;
+}
---
+++ ppc_problems/math_ad_hoc/trigo_tabuleiro.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int test;
+	cin >> test;
+
+	for(int i=0 ; i<test ; i++){
+		int qnt;
+		cin >> qnt;
+		unsigned long long e = 1;
+
+		for(int j=1; j<qnt ; j++){
+			e *= 2;
+		}
+
+		cout << (e/6)/1000 << " kg" << endl;
+	}
+	return 0;
+}
