a2120350a14e96ab23fba8c38211ba57aabac2c0
Paulo Tada <paulohtfs@gmail.com>
2015-06-10 16:53:32 -0300

    Fixing primo.cpp and adding test file

--- algorithms/paulo/primo.cpp
+++ algorithms/paulo/primo.cpp
@@ -8,13 +8,14 @@ int is_primo(int n){
 	if(n==2) return true;
 	if((n&1)==0) return false;
 	for(int i=3; i< sqrt(n); i+=2){
-		if((n&(i-1))==0) return false;
+		if((n%i)==0) return false;
 	}
 	return true;
 }
 
 int main() {
 	int number;
-	cin >> number;
-	cout << is_primo(number) << endl;
+	while(cin >> number){
+		cout << is_primo(number) << endl;
+	}
 }
