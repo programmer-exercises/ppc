c5305be850f96823a18b7ad4df48273fb02e1f0c
Paulo Tada <paulohtfs@gmail.com>
2015-06-16 06:34:33 -0300

    Erastotenes and fatoracao

--- algorithms/paulo/erastotenes.cpp
+++ algorithms/paulo/erastotenes.cpp
@@ -6,21 +6,26 @@ using namespace std;
 #define MAX 10000
 
 bool sieve[MAX+1];
-inf pdimoz[MAX];
+inf pdimos[MAX];
 
 void precomp(){
 	memset(sieve,0xff,(MAX+1)*sizeof(bool));
 	sieve[0] = false;
 	sieve[1] = false;
+	int i, j;
+	int count = 0;
+	primos[count++];
 
 	int limit = sqrt(MAX);
-	for (int i = 3; i*i <= limit; i+=2){
+	for ( i = 3; i*i <= limit; i+=2){
 		if(sieve[i]){
-			for (int j = i*i; j <= MAX ; j+=i){
+			primos[count++] = i;
+			for (j = i*i; j <= MAX ; j+=i){
 				sieve[j] = false;
 			}
 		}
-	}
+	
+	for()
 }
 
 int main(){
